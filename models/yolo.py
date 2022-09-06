a6041273d39d471a47eaf78dcdbc4afb029a3faa
triple-Mu <gpu@163.com>
2022-09-06 13:52:25 +0800

    Add dehead

--- models/yolo.py
+++ models/yolo.py
@@ -87,6 +87,59 @@ class Detect(nn.Module):
         return grid, anchor_grid
 
 
+class Decoupled_Detect(nn.Module):
+    stride = None  # strides computed during build
+    onnx_dynamic = False  # ONNX export parameter
+    export = False  # export mode
+
+    def __init__(self, nc=80, anchors=(), ch=(), inplace=True):  # detection layer
+        super().__init__()
+
+        self.nc = nc  # number of classes
+        self.no = nc + 5  # number of outputs per anchor
+        self.nl = len(anchors)  # number of detection layers
+        self.na = len(anchors[0]) // 2  # number of anchors
+        self.grid = [torch.zeros(1)] * self.nl  # init grid
+        self.anchor_grid = [torch.zeros(1)] * self.nl  # init anchor grid
+        self.register_buffer('anchors', torch.tensor(anchors).float().view(self.nl, -1, 2))  # shape(nl,na,2)
+        self.m = nn.ModuleList(DecoupledHead(x, nc, anchors) for x in ch)
+        self.inplace = inplace  # use in-place ops (e.g. slice assignment)
+
+    def forward(self, x):
+        z = []  # inference output
+        for i in range(self.nl):
+            x[i] = self.m[i](x[i])  # conv
+            bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
+            x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
+
+            if not self.training:  # inference
+                if self.onnx_dynamic or self.grid[i].shape[2:4] != x[i].shape[2:4]:
+                    self.grid[i], self.anchor_grid[i] = self._make_grid(nx, ny, i)
+
+                y = x[i].sigmoid()
+                if self.inplace:
+                    y[..., 0:2] = (y[..., 0:2] * 2 + self.grid[i]) * self.stride[i]  # xy
+                    y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
+                else:  # for YOLOv5 on AWS Inferentia https://github.com/ultralytics/yolov5/pull/2953
+                    xy, wh, conf = y.split((2, 2, self.nc + 1), 4)  # y.tensor_split((2, 4, 5), 4)  # torch 1.8.0
+                    xy = (xy * 2 + self.grid[i]) * self.stride[i]  # xy
+                    wh = (wh * 2) ** 2 * self.anchor_grid[i]  # wh
+                    y = torch.cat((xy, wh, conf), 4)
+                z.append(y.view(bs, -1, self.no))
+
+        return x if self.training else (torch.cat(z, 1),) if self.export else (torch.cat(z, 1), x)
+
+    def _make_grid(self, nx=20, ny=20, i=0, torch_1_10=check_version(torch.__version__, '1.10.0')):
+        d = self.anchors[i].device
+        t = self.anchors[i].dtype
+        shape = 1, self.na, ny, nx, 2  # grid shape
+        y, x = torch.arange(ny, device=d, dtype=t), torch.arange(nx, device=d, dtype=t)
+        yv, xv = torch.meshgrid(y, x, indexing='ij') if torch_1_10 else torch.meshgrid(y, x)  # torch>=0.7 compatibility
+        grid = torch.stack((xv, yv), 2).expand(shape) - 0.5  # add grid offset, i.e. y = 2.0 * x - 0.5
+        anchor_grid = (self.anchors[i] * self.stride[i]).view((1, self.na, 1, 1, 2)).expand(shape)
+        return grid, anchor_grid
+
+
 class BaseModel(nn.Module):
     # YOLOv5 base model
     def forward(self, x, profile=False, visualize=False):
@@ -178,6 +231,15 @@ class DetectionModel(BaseModel):
             self.stride = m.stride
             self._initialize_biases()  # only run once
 
+        elif isinstance(m, Decoupled_Detect):
+            s = 256  # 2x min stride
+            m.inplace = self.inplace
+            m.stride = torch.tensor([s / x.shape[-2] for x in self.forward(torch.empty(1, ch, s, s))])  # forward
+            check_anchor_order(m)  # must be in pixel-space (not grid-space)
+            m.anchors /= m.stride.view(-1, 1, 1)
+            self.stride = m.stride
+            self._initialize_dh_biases()  # only run once
+
         # Init weights, biases
         initialize_weights(self)
         self.info()
@@ -240,6 +302,19 @@ class DetectionModel(BaseModel):
             b[:, 5:] += math.log(0.6 / (m.nc - 0.999999)) if cf is None else torch.log(cf / cf.sum())  # cls
             mi.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
 
+    def _initialize_dh_biases(self, cf=None):  # initialize biases into Detect(), cf is class frequency
+        # https://arxiv.org/abs/1708.02002 section 3.3
+        # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1.
+        m = self.model[-1]  # Detect() module
+        for mi, s in zip(m.m, m.stride):  # from
+            reg_bias = mi.reg_preds.bias.view(m.na, -1).detach()
+            reg_bias += math.log(8 / (640 / s) ** 2)
+            mi.reg_preds.bias = torch.nn.Parameter(reg_bias.view(-1), requires_grad=True)
+
+            cls_bias = mi.cls_preds.bias.view(m.na, -1).detach()
+            cls_bias += math.log(0.6 / (m.nc - 0.999999)) if cf is None else torch.log(cf / cf.sum())  # cls
+            mi.cls_preds.bias = torch.nn.Parameter(cls_bias.view(-1), requires_grad=True)
+
 
 Model = DetectionModel  # retain YOLOv5 'Model' class for backwards compatibility
 
@@ -300,7 +375,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
             c2 = sum(ch[x] for x in f)
         elif m is Concat_bifpn:
             c2 = max(ch[x] for x in f)
-        elif m is Detect:
+        elif m in [Detect, Decoupled_Detect]:
             args.append([ch[x] for x in f])
             if isinstance(args[1], int):  # number of anchors
                 args[1] = [list(range(args[1] * 2))] * len(f)
@@ -337,12 +412,14 @@ if __name__ == '__main__':
     print_args(vars(opt))
     device = select_device(opt.device)
 
-    # s = 'exp/yolov5x6_4up_huge.yaml'                           # YOLOv5x6_4up summary: 845 layers, 142577475 parameters, 142577475 gradients, 228.3 GFLOPs
-    s = 'hub/yolov5x6.yaml'                               # YOLOv5x6 summary: 733 layers, 140794780 parameters, 140794780 gradients, 210.5 GFLOPs
-    # s = 'exp/yolov5x6_4up_large_bifpn.yaml'  # YOLOv5x6_4up_large_bifpn summary: 824 layers, 117698356 parameters, 117698356 gradients
-    # s = 'exp/yolov5x6_4up_large.yaml'                     # YOLOv5x6_4up_large summary: 789 layers, 117690300 parameters, 117690300 gradients
+    # s = 'exp/yolov5x6_4up_huge.yaml'                          # YOLOv5x6_4up summary:                 845 layers, 142577475 parameters, 142577475 gradients, 228.3 GFLOPs
+    # s = 'exp/yolov5x6_4up_large_bifpn.yaml'                   # YOLOv5x6_4up_large_bifpn summary:     824 layers, 117698356 parameters, 117698356 gradients
+    # s = 'exp/yolov5x6_4up_large.yaml'                         # YOLOv5x6_4up_large summary:           789 layers, 117690300 parameters, 117690300 gradients
     # s = 'exp/yolov5x6_bifpn.yaml'
-    # s = 'exp/yolov5x6_4up_huge_bifpn.yaml'
+    # s = 'exp/yolov5x6_4up_huge_bifpn.yaml'                    # YOLOv5x6_4up_huge_bifpn summary:      885 layers, 150403899 parameters, 150403899 gradients, 260.5 GFLOPs
+    # s = 'hub/yolov5x6.yaml'                                   # YOLOv5x6 summary:                     733 layers, 140794780 parameters, 140794780 gradients, 210.5 GFLOPs
+    s = 'exp/yolov5x6_dhead.yaml'  # YOLOv5x6_dhead summary:               825 layers, 150506524 parameters, 150506524 gradients, 251.8 GFLOPs
+
     opt.cfg = s
     # Create model
     im = torch.rand(opt.batch_size, 3, 1600, 1600).to(device)
