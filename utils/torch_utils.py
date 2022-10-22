533da1ce55f42f47811f4617070bb43d3bf51a49
triple-Mu <gpu@163.com>
2022-10-22 11:45:13 +0800

    Fix optimizer and nms time limit

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -327,19 +327,19 @@ def smart_optimizer(model, name='Adam', lr=0.001, momentum=0.9, decay=1e-5):
             else:
                 g[0].append(p)  # weight (with decay)
 
+    optimizer_list = [{'params': g[0], 'weight_decay': decay}, {'params': g[1]}, {'params': g[2]}]
+
     if name == 'Adam':
-        optimizer = torch.optim.Adam(g[2], lr=lr, betas=(momentum, 0.999))  # adjust beta1 to momentum
+        optimizer = torch.optim.Adam(optimizer_list, lr=lr, betas=(momentum, 0.999))  # adjust beta1 to momentum
     elif name == 'AdamW':
-        optimizer = torch.optim.AdamW(g[2], lr=lr, betas=(momentum, 0.999), weight_decay=0.0)
+        optimizer = torch.optim.AdamW(optimizer_list, lr=lr, betas=(momentum, 0.999), weight_decay=0.0)
     elif name == 'RMSProp':
-        optimizer = torch.optim.RMSprop(g[2], lr=lr, momentum=momentum)
+        optimizer = torch.optim.RMSprop(optimizer_list, lr=lr, momentum=momentum)
     elif name == 'SGD':
-        optimizer = torch.optim.SGD(g[2], lr=lr, momentum=momentum, nesterov=True)
+        optimizer = torch.optim.SGD(optimizer_list, lr=lr, momentum=momentum, nesterov=True)
     else:
         raise NotImplementedError(f'Optimizer {name} not implemented.')
 
-    optimizer.add_param_group({'params': g[0], 'weight_decay': decay})  # add g0 with weight_decay
-    optimizer.add_param_group({'params': g[1], 'weight_decay': 0.0})  # add g1 (BatchNorm2d weights)
     LOGGER.info(f"{colorstr('optimizer:')} {type(optimizer).__name__}(lr={lr}) with parameter groups "
                 f"{len(g[1])} weight(decay=0.0), {len(g[0])} weight(decay={decay}), {len(g[2])} bias")
     return optimizer
